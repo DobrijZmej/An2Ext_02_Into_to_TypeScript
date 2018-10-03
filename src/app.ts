d21d3c5169695c335886a8ef1d00fe83b9b923e6
DobrijZmej <DobrijZmej@gmail.com>
2018-10-03 21:50:38 +0300

    Урок 1. Задание 2.

--- src/app.ts
+++ src/app.ts
@@ -29,8 +29,45 @@ function logFirstAvailable(books: bookType[]): void {
         }
     }
 }
-logFirstAvailable(getAllBooks());
+let books: bookType[] = getAllBooks();
+console.log(`First stage get array of books:`);
+console.log(books);
+logFirstAvailable(books);
 
 /**************** Задание 02 ************************/
 
 enum Category {'JavaScript', 'CSS', 'HTML', 'TypeScript', 'Angular'};
+interface bookType2 {title: string, author: string, available?: boolean, category?: Category};
+
+function addCategory2Books(books: bookType[]): bookType[] {
+    let booksNew: bookType2[] = [];
+    for(let book of books) {
+        booksNew.push(<bookType2>book);
+        booksNew[booksNew.length-1].category = Math.floor(Math.random()*5);
+    }
+    return booksNew;
+}
+books = addCategory2Books(books);
+console.log(`Added categories to array books:`);
+console.log(books);
+
+function getBookTitlesByCategory(books: bookType2[], category: Category): bookType2[] {
+    let booksNew: bookType2[] = [];
+    for(let book of books) {
+        if(book.category === category) {
+            booksNew.push(book);
+        }
+    }
+    return booksNew;
+}
+const categoryFilter = Math.floor(Math.random()*5);
+console.log(`Filter books by category ${ categoryFilter } (${ Category[categoryFilter] })`);
+console.log(getBookTitlesByCategory(books, categoryFilter));
+
+function logBookTitles(books: bookType[]): void {
+    console.log(books.length);
+    for(let book of books) {
+        console.log(book.title);
+    }
+}
+logBookTitles(books);
